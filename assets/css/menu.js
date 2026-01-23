fa99c40f3ee574581efd48a5a05dfaf8de2c6068
Dereck Mungoriwo <dereck-mungoriwo@outlook.com>
2026-01-23 11:58:46 +0200

    Delete assets/css directory

--- assets/css/menu.js
+++
@@ -1,5 +0,0 @@
-const menuItems = [
-  { id: 1, name: "Classic Burger", description: "Beef patty, lettuce, cheese", price: 8.99, ingredients: { beef_patty:1, burger_bun:1, lettuce:1, cheese:1 }},
-  { id: 2, name: "Chicken Wrap", description: "Grilled chicken wrap", price: 7.49, ingredients: { chicken:1, wrap:1, lettuce:1 }},
-  { id: 3, name: "French Fries", description: "Crispy fries", price: 3.50, ingredients: { potato:3 }}
-];
